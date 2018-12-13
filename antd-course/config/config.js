bd100d22ca787bfd55bcf2e244e75072a748c672
e <569653441@qq.com>
2018-12-13 15:03:28 +0800

    antd layout routes

--- antd-course/config/config.js
+++ antd-course/config/config.js
@@ -3,7 +3,34 @@ export default {
     routes: [{
         path: '/',
         // 这里配置了路径  将根路径'/'，改为helloworld的路径了
-        component: './HelloWorld',
+        component: '../layout',
+        routes: [
+            {
+                path: '/',
+                component: 'Helloworld',
+            },
+            {
+              path: '/helloworld',
+              component: 'Helloworld',
+            },
+            {
+              path: '/dashboard',
+              routes: [
+                  {
+                    path: '/dashboard/analysis',
+                    component: 'DashBoard/Analysis',
+                  },
+                  {
+                      path: '/dashboard/monitor',
+                      component: 'DashBoard/Monitor',
+                  },
+                  {
+                      path: '/dashboard/workplace',
+                      component: 'DashBoard/Workplace'
+                  },
+              ],
+            },
+        ]
     }],
     plugins: [
         ['umi-plugin-react', {
